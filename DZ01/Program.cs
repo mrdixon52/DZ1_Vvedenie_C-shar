59b80da0b96a6e89d3eca18eaa42bf51b9bec0bb
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-23 19:16:06 +0300

    DZ01 Done

--- DZ01/Program.cs
+++ DZ01/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+else 
+{
+    max = a;
+    min = b;
+}
+Console.Write("Largest of 2 numbers = "); 
+Console.Write(max);
+Console.WriteLine();
+Console.Write("Smallest of 2 numbers = "); 
+Console.Write(min);
