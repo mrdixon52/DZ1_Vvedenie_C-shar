218fac11f81eaeaf73bfbf74df822a4705bf54d5
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-23 20:00:23 +0300

    DZ03 Done

---
+++ DZ03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double check = n % 2;
+if (check == 0) 
+{
+    Console.WriteLine("Even number"); // четное число
+}
+else 
+{
+    Console.WriteLine("Odd number"); // нечетное число
+}
