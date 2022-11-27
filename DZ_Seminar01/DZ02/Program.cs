bd37a67be5ffe818a6e9ab005da60c5389567d08
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-11-27 20:22:35 +0300

    add folders with dz_seminars

---
+++ DZ_Seminar01/DZ02/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int max1 = 0;
+
+if (a < b)
+{
+    max = b;
+}
+else 
+{
+    max = a;
+}
+
+if (max < c) 
+{
+    max1 = c;
+}
+else 
+{
+    max1 = max;
+}
+Console.Write("Largest of 3 numbers = "); 
+Console.Write(max1);
